13f195bbed7a70e7d593159347a0f6f89d3e87e4
Valyo0430 <72649987+zzvalkatazz@users.noreply.github.com>
2024-04-26 16:58:55 +0300

    Add files via upload

---
+++ task4.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float num1,num2,num3;
+    cout<<"Insert numbers"<<endl;
+    cin>>num1>>num2>>num3;
+    double maxNum=num1;
+    if(num2>maxNum)
+    {
+        maxNum=num2;
+    }
+    if(num3>maxNum)
+    {
+        maxNum=num3;
+    }
+   cout<<"Max number is:"<<maxNum<<endl;
+   return 0;
+}
+
+// on TERMINAL 
+// first we create file.txt
+// echo 21.3 22.7 20.5 > file.txt
+//these are 3 numbers 
+// in same directory we save file.txt and task4.cpp
+// compile : g++ -o task4 task4.cpp
+// ./task4<file.txt
+//output:
+//Insert numbers
+// Max number is 22.7
